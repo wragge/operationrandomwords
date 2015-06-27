3a21dac06ecde88b13e313da487511546bc360f3
Tim Sherratt <tim@discontents.com.au>
2015-06-27 17:47:07 +1000

    Minor style edits

--- operationweb.py
+++ operationweb.py
@@ -8,31 +8,34 @@ app = Flask(__name__)
 
 p = inflect.engine()
 with open('nouns.pck', 'rb') as noun_file:
-	nouns = pickle.load(noun_file)
+    nouns = pickle.load(noun_file)
 with open('adjs.pck', 'rb') as adj_file:
-	adjs = pickle.load(adj_file)
+    adjs = pickle.load(adj_file)
 with open('verbs.pck', 'rb') as verb_file:
-	verbs = pickle.load(verb_file)
+    verbs = pickle.load(verb_file)
 
-phrases = [u'protecting Australia from',
-	u'defending Australia’s',
-	u'returning Australia’s',
-	u'{} illegal']
+phrases = [
+    u'protecting Australia from',
+    u'defending Australia’s',
+    u'returning Australia’s',
+    u'{} illegal'
+    ]
+
 
 def create_message():
-	return (u'Operation {} {}'.format(
-			random.choice(adjs).title(), 
-			random.choice(nouns).title()),
-		u'{} {}'.format(
-			random.choice(phrases).format(p.present_participle(random.choice(verbs))),
-			p.plural(random.choice(nouns)))
-		)
+    return (u'Operation {} {}'.format(
+            random.choice(adjs).title(),
+            random.choice(nouns).title()),
+            u'{} {}'.format(
+            random.choice(phrases).format(p.present_participle(random.choice(verbs))),
+            p.plural(random.choice(nouns)))
+            )
+
 
 @app.route('/')
 def operation():
-	operation, tagline = create_message()
-	return render_template('index.html', operation=operation, tagline=tagline)
+    operation, tagline = create_message()
+    return render_template('index.html', operation=operation, tagline=tagline)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
